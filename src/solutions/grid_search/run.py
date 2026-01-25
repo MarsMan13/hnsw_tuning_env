77de091267ac1a7a3c21174ee95a222fbb4ac1be
MarsMan13 <choigeon0513@gmail.com>
2026-01-25 16:13:27 +0900

    For artifact

--- src/solutions/grid_search/run.py
+++ src/solutions/grid_search/run.py
@@ -32,37 +32,3 @@ def run(impl=IMPL, dataset=DATASET, recall_min=None, qps_min=None, tuning_budget
             break
         results.append(((M, efC, efS), (gd.tuning_time, recall, qps, total_time, build_time, index_size)))
     return results
-
-def recall_min():    
-    for RECALL_MIN in [0.90, 0.95, 0.975]:
-        for IMPL in ["hnswlib", "faiss"]:
-            for DATASET in ["nytimes-256-angular", "sift-128-euclidean", "glove-100-angular", "dbpediaentity-768-angular", "msmarco-384-angular", "youtube-1024-angular"]:
-                results = run(IMPL, DATASET, RECALL_MIN, TUNING_BUDGET)
-                print_optimal_hyperparameters(results, recall_min=RECALL_MIN)
-                postprocess_results(
-                    results,
-                    solution="grid_search",
-                    impl=IMPL,
-                    dataset=DATASET,
-                    recall_min=RECALL_MIN,
-                    tuning_budget=TUNING_BUDGET
-                )
-
-def qps_min():
-    for QPS_MIN in [2500, 5000, 10000, 25000]:
-        for IMPL in ["hnswlib", "faiss"]:
-            for DATASET in ["nytimes-256-angular", "sift-128-euclidean", "glove-100-angular", "dbpediaentity-768-angular", "msmarco-384-angular", "youtube-1024-angular"]:
-                results = run(IMPL, DATASET, qps_min=QPS_MIN, tuning_budget=TUNING_BUDGET)
-                print_optimal_hyperparameters(results, qps_min=QPS_MIN)
-                postprocess_results(
-                    results,
-                    solution="grid_search",
-                    impl=IMPL,
-                    dataset=DATASET,
-                    qps_min=QPS_MIN,
-                    tuning_budget=TUNING_BUDGET
-                )
-
-if __name__ == "__main__":
-    recall_min()
-    qps_min()
